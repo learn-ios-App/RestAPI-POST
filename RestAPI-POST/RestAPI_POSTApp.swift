af69349a66c2669b58bf220e666f271a80329cc9
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-11 21:02:46 +0900

    Initial Commit

---
+++ RestAPI-POST/RestAPI_POSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestAPI_POSTApp.swift
+//  RestAPI-POST
+//
+//  Created by 渡邊魁優 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct RestAPI_POSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
